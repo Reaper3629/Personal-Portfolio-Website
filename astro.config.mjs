0ba6850c3a41b6d0bac4d50ae76789a17d3a9968
Reaper3629 <ap7552897@gmail.com>
2024-07-19 12:54:31 -0700

    added tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Reaper3629.github.io',
+  site: 'https://Reaper3629.github.io',
+  integrations: [tailwind()]
 });
